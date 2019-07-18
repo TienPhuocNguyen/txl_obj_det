63b5a8a7127ff0bfd41d1d765414030464c77450
Tien Nguyen <tiennp51@gmail.com>
2019-07-18 17:31:45 +0900

    Add files via upload

---
+++ feature_matching.py
@@ -0,0 +1,261 @@
+import cv2 as cv
+import numpy as np
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+def sift(input1, input2):
+    # Feature detection and mathching
+    sift = cv.xfeatures2d.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(input1, None)
+    kp2, des2 = sift.detectAndCompute(input2, None)
+    
+    idx_params    = dict(algorithm = 0, trees = 5)
+    search_params = dict(checks=50)
+    
+    flann = cv.FlannBasedMatcher(idx_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+    
+    good, pts1, pts2 = [], [], []
+    for  i, (m, n) in enumerate(matches):
+        if m.distance < 0.8 * n.distance:
+            good.append(m)
+            pts2.append(kp2[m.trainIdx].pt)
+            pts1.append(kp1[m.queryIdx].pt)
+
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    
+    # Find homography and remove outliers
+    M, mask = cv.findHomography(pts1, pts2, cv.RANSAC, 0.5)
+    matchesMask = mask.ravel().tolist()
+    pts1 = pts1[matchesMask]
+    pts2 = pts2[matchesMask]
+    
+    return pts1, kp1, des1, pts2, kp2, des2, good, matchesMask, M
+    
+
+# Define network
+class SuperPointNet(torch.nn.Module):
+  """ Pytorch definition of SuperPoint Network. """
+  def __init__(self):
+    super(SuperPointNet, self).__init__()
+    self.relu = torch.nn.ReLU(inplace=True)
+    self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+    c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+    # Shared Encoder.
+    self.conv1a = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
+    self.conv1b = torch.nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
+    self.conv2a = torch.nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
+    self.conv2b = torch.nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
+    self.conv3a = torch.nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
+    self.conv3b = torch.nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
+    self.conv4a = torch.nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
+    self.conv4b = torch.nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
+    # Detector Head.
+    self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+    self.convPb = torch.nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
+    # Descriptor Head.
+    self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+    self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+
+  def forward(self, x):
+    """ Forward pass that jointly computes unprocessed point and descriptor
+    tensors.
+    Input
+      x: Image pytorch tensor shaped N x 1 x H x W.
+    Output
+      semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
+      desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+    """
+    # Shared Encoder.
+    x = self.relu(self.conv1a(x))
+    x = self.relu(self.conv1b(x))
+    x = self.pool(x)
+    x = self.relu(self.conv2a(x))
+    x = self.relu(self.conv2b(x))
+    x = self.pool(x)
+    x = self.relu(self.conv3a(x))
+    x = self.relu(self.conv3b(x))
+    x = self.pool(x)
+    x = self.relu(self.conv4a(x))
+    x = self.relu(self.conv4b(x))
+    # Detector Head.
+    cPa = self.relu(self.convPa(x))
+    semi = self.convPb(cPa)
+    # Descriptor Head.
+    cDa = self.relu(self.convDa(x))
+    desc = self.convDb(cDa)
+    dn = torch.norm(desc, p=1, dim=1) # Compute the norm. - error in ONNX
+    desc = desc.div(torch.unsqueeze(dn, 1)) # Divide by norm to normalize.
+    return semi, desc
+
+def nms_fast(in_corners, H, W, dist_thresh):
+    """
+    Run a faster approximate Non-Max-Suppression on numpy corners shaped:
+      3xN [x_i,y_i,conf_i]^T
+  
+    Algo summary: Create a grid sized HxW. Assign each corner location a 1, rest
+    are zeros. Iterate through all the 1's and convert them either to -1 or 0.
+    Suppress points by setting nearby values to 0.
+  
+    Grid Value Legend:
+    -1 : Kept.
+     0 : Empty or suppressed.
+     1 : To be processed (converted to either kept or supressed).
+  
+    NOTE: The NMS first rounds points to integers, so NMS distance might not
+    be exactly dist_thresh. It also assumes points are within image boundaries.
+  
+    Inputs
+      in_corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+      H - Image height.
+      W - Image width.
+      dist_thresh - Distance to suppress, measured as an infinty norm distance.
+    Returns
+      nmsed_corners - 3xN numpy matrix with surviving corners.
+      nmsed_inds - N length numpy vector with surviving corner indices.
+    """
+    grid = np.zeros((H, W)).astype(int) # Track NMS data.
+    inds = np.zeros((H, W)).astype(int) # Store indices of points.
+    # Sort by confidence and round to nearest int.
+    inds1 = np.argsort(-in_corners[2,:])
+    corners = in_corners[:,inds1]
+    rcorners = corners[:2,:].round().astype(int) # Rounded corners.
+    # Check for edge case of 0 or 1 corners.
+    if rcorners.shape[1] == 0:
+      return np.zeros((3,0)).astype(int), np.zeros(0).astype(int)
+    if rcorners.shape[1] == 1:
+      out = np.vstack((rcorners, in_corners[2])).reshape(3,1)
+      return out, np.zeros((1)).astype(int)
+    # Initialize the grid.
+    for i, rc in enumerate(rcorners.T):
+      grid[rcorners[1,i], rcorners[0,i]] = 1
+      inds[rcorners[1,i], rcorners[0,i]] = i
+    # Pad the border of the grid, so that we can NMS points near the border.
+    pad = dist_thresh
+    grid = np.pad(grid, ((pad,pad), (pad,pad)), mode='constant')
+    # Iterate through points, highest to lowest conf, suppress neighborhood.
+    count = 0
+    for i, rc in enumerate(rcorners.T):
+      # Account for top and left padding.
+      pt = (rc[0]+pad, rc[1]+pad)
+      if grid[pt[1], pt[0]] == 1: # If not yet suppressed.
+        grid[pt[1]-pad:pt[1]+pad+1, pt[0]-pad:pt[0]+pad+1] = 0
+        grid[pt[1], pt[0]] = -1
+        count += 1
+    # Get all surviving -1's and return sorted array of remaining corners.
+    keepy, keepx = np.where(grid==-1)
+    keepy, keepx = keepy - pad, keepx - pad
+    inds_keep = inds[keepy, keepx]
+    out = corners[:, inds_keep]
+    values = out[-1, :]
+    inds2 = np.argsort(-values)
+    out = out[:, inds2]
+    out_inds = inds1[inds_keep[inds2]]
+    return out, out_inds
+
+def run(inp):
+    H = inp.shape[0]
+    W = inp.shape[1]
+    cell = 8
+    conf_thresh = 0.015
+    nms_dist = 4
+    border_remove = 4
+    
+    net = SuperPointNet()
+    net.load_state_dict(torch.load("superpoint_v1.pth"))
+    net.eval()
+
+    # process image to extract points and descriptors
+    inp = (inp.reshape(1, H, W))
+    inp = torch.from_numpy(inp)
+    inp = torch.autograd.Variable(inp).view(1, 1, H, W)
+    # Forward pass of network.
+    outs = net.forward(inp)
+    semi, coarse_desc = outs[0], outs[1]
+    # Convert pytorch -> numpy.
+    semi = semi.data.cpu().numpy().squeeze()
+    
+    # --- Process points.
+    dense = np.exp(semi) # Softmax.
+    dense = dense / (np.sum(dense, axis=0)+.00001) # Should sum to 1.
+    
+    # Remove dustbin.
+    nodust = dense[:-1, :, :]
+    # Reshape to get full resolution heatmap.
+    Hc = int(H / cell)
+    Wc = int(W / cell)
+    nodust = nodust.transpose(1, 2, 0)
+    heatmap = np.reshape(nodust, [Hc, Wc, cell, cell])
+    heatmap = np.transpose(heatmap, [0, 2, 1, 3])
+    heatmap = np.reshape(heatmap, [Hc*cell, Wc*cell])
+    xs, ys = np.where(heatmap >= conf_thresh) # Confidence threshold.
+    
+    pts = np.zeros((3, len(xs))) # Populate point data sized 3xN.
+    pts[0, :] = ys
+    pts[1, :] = xs
+    pts[2, :] = heatmap[xs, ys]
+    pts, _ = nms_fast(pts, H, W, dist_thresh=nms_dist) # Apply NMS.
+    inds = np.argsort(pts[2,:])
+    pts = pts[:,inds[::-1]] # Sort by confidence.
+    # Remove points along border.
+    bord = border_remove
+    toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W-bord))
+    toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H-bord))
+    toremove = np.logical_or(toremoveW, toremoveH)
+    pts = pts[:, ~toremove]
+    # --- Process descriptor.
+    D = coarse_desc.shape[1]
+
+    if pts.shape[1] == 0:
+        desc = np.zeros((D, 0))
+    else:
+        # Interpolate into descriptor map using 2D point locations.
+        samp_pts = torch.from_numpy(pts[:2, :].copy())
+        samp_pts[0, :] = (samp_pts[0, :] / (float(W)/2.)) - 1.
+        samp_pts[1, :] = (samp_pts[1, :] / (float(H)/2.)) - 1.
+        samp_pts = samp_pts.transpose(0, 1).contiguous()
+        samp_pts = samp_pts.view(1, 1, -1, 2)
+        samp_pts = samp_pts.float()
+    
+        desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
+        desc = desc.data.cpu().numpy().reshape(D, -1)
+        desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
+    
+    return pts, desc, heatmap
+
+def nn_match_two_way(desc1, desc2, nn_thresh=0.3):
+    
+    assert desc1.shape[0] == desc2.shape[0]
+    if desc1.shape[1] == 0 or desc2.shape[1] == 0:
+      return np.zeros((3, 0))
+    if nn_thresh < 0.0:
+      raise ValueError('\'nn_thresh\' should be non-negative')
+    # Compute L2 distance. Easy since vectors are unit normalized.
+    dmat = np.dot(desc1.T, desc2)
+    dmat = np.sqrt(2-2*np.clip(dmat, -1, 1))
+    # Get NN indices and scores.
+    idx = np.argmin(dmat, axis=1)
+    scores = dmat[np.arange(dmat.shape[0]), idx]
+    # Threshold the NN matches.
+    keep = scores < nn_thresh
+    # Check if nearest neighbor goes both directions and keep those.
+    idx2 = np.argmin(dmat, axis=0)
+    keep_bi = np.arange(len(idx)) == idx2[idx]
+    keep = np.logical_and(keep, keep_bi)
+    idx = idx[keep]
+    scores = scores[keep]
+    # Get the surviving point indices.
+    m_idx1 = np.arange(desc1.shape[1])[keep]
+    m_idx2 = idx
+    # Populate the final 3xN match data structure.
+    matches = np.zeros((3, int(keep.sum())))
+    matches[0, :] = m_idx1
+    matches[1, :] = m_idx2
+    matches[2, :] = scores
+    matches = matches[:, matches[2, :].argsort()]
+    matches = matches[:, :]
+#     print(matches[2,:])
+    return matches
